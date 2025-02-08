aa3a8a33155c2539841a0b6d9cde506ab130a8e3
Tiago Ramon <tiagoramon.sousa12@gmail.com>
2025-02-07 21:10:29 -0300

    ajeitando os commits

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [task, setTask] = useState<string[]>([]);
+  const [edit, setEdit] = useState({
+    enable: false,
+    task: "",
+  });
+
+  const handleRegister = () => {
+    if (!input) {
+      alert("Preencha o nome da sua tarefa");
+      return;
+    }
+    if (edit.enable) {
+      handleSaveEdit();
+      return;
+    }
+
+    setTask((tarefa) => [...tarefa, input]);
+    setInput("");
+  };
+
+  const handleSaveEdit = () => {
+    const findIndexTask = task.findIndex((task) => task === edit.task);
+    const allTask = [...task];
+    allTask[findIndexTask] = input;
+    setTask(allTask);
+    console.log(allTask);
+    console.log(setTask);
+
+    setEdit({
+      enable: false,
+      task: "",
+    });
+    setInput("");
+  };
+
+  const handleDelete = (item: string) => {
+    const removeTask = task.filter((task) => task !== item);
+    setTask(removeTask);
+  };
+
+  const handleEdit = (item: string) => {
+    setInput(item);
+    setEdit({
+      enable: true,
+      task: item,
+    });
+  };
+
+  return (
+    <div>
+      <h1>Lista de tarefas</h1>
+      <input
+        placeholder="Digite o nome da tarefa..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleRegister}>
+        {edit.enable ? "Atualizar tarefa" : "Adicionar tarefa"}
+      </button>
+      <hr />
+      {task.map((item, index) => (
+        <section key={item}>
+          <span>{item}</span>
+          <button onClick={() => handleEdit(item)}>Editar</button>
+          <button onClick={() => handleDelete(item)}>Excluir</button>
+        </section>
+      ))}
+    </div>
+  );
+}
+
+export default App;
