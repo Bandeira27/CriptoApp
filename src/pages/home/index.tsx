f7f1371fab0363309a72dca7f11eeff4129f8acd
Tiago Ramon <tiagoramon.sousa12@gmail.com>
2025-03-04 14:38:13 -0300

    Funcionalidades do formulário

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,32 @@
+import { useState, FormEvent } from "react";
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Home() {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    navigate(`./detail/${input}`);
+  };
+
+  const handleGetMore = () => [];
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Digite o nome da moeda..." />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o nome da moeda..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">
           <BsSearch size={30} color="#FFF" />
         </button>
@@ -51,6 +71,10 @@ export function Home() {
           </tr>
         </tbody>
       </table>
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais
+      </button>
     </main>
   );
 }
